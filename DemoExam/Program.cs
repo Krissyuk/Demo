0a8dcc03076de899f26df3036140d23cf50dfa02
Kristina_Brelova <krisbrelova838@gmail.com>
2024-11-21 22:23:48 +0500

    Полностью законченный Бэкенд

--- DemoExam/Program.cs
+++ DemoExam/Program.cs
@@ -1,30 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+
 List<Order> repo =
 [
-    new(0,new(2005,3,12),"холодос","Не морозит","Не морозит совсем","Брелова Кристина","Не назначен"),
+    new(1,new(2005,3,12),"холодос","Не морозит","Не морозит совсем","Брелова Кристина","Не назначен"),
 ];
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 builder.Services.AddCors();
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseCors(a => a.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-app.MapGet("/", () => "Hello World!");
+string message = "";
+
+app.MapGet("orders", (int param = 0) =>
+{
+    string buffer = message;
+    message = "";
+    if (param != 0)
+        return new {repo = repo.FindAll(x=>x.Number==param), message = buffer};
+    return new { repo, message = buffer };
+});
+
+app.MapGet("create", ([AsParameters] Order dto) =>
+    repo.Add(dto));
+
+app.MapGet("update", ([AsParameters] UpdateOrderDTO dto) => 
+{ 
+    var o = repo.Find(x => x.Number == dto.Number);
+    if (o == null)
+        return;
+    if (dto.Status != o.Status && dto.Status != "")
+    {
+        o.Status = dto.Status;
+        message += $"Статус заявки №{o.Number} изменен\n";
+        if (o.Status == "Выполнено")
+        {
+            message += $"Заявка №{o.Number} завершена\n";
+            o.EndDate = DateOnly.FromDateTime(DateTime.Now);
+        }
+    }
+    if (dto.Description != "")
+        o.Description = dto.Description;
+    if (dto.Master != "")
+        o.Master = dto.Master;
+    if (dto.Comment != "")
+        o.Comment.Add(dto.Comment);
+});
+
+int complete_count() => repo.FindAll(x => x.Status == "выполнено").Count;
+
+Dictionary<string, int> get_problem_type_stat() =>
+    repo.GroupBy(x => x.ProblemType)
+    .Select(x => (x.Key, x.Count()))
+    .ToDictionary(k => k.Key, v => v.Item2);
+
+double get_average_time_to_complete() =>
+    complete_count() == 0 ? 0 :
+    repo.FindAll(x => x.Status == "Выполнено")
+    .Select(x => x.EndDate.Value.DayNumber - x.StartDate.DayNumber)
+    .Sum() / complete_count();
+
+app.MapGet("/statistics", () =>new{
+    complete_count = complete_count(),
+    problem_type_stat = get_problem_type_stat(),
+    average_time_to_complete = get_average_time_to_complete()
+});
 
 app.Run();
 
-class Order(int number, DateTime startDate, string device, string problem, string fullproblem, string fullname, string statys)
+class Order(int number, DateOnly startDate, string device, string problemtype, string Description, string fullname, string status)
 {
     public int Number { get; set; } = number;
-    public DateTime StartDate { get; set; } = startDate;
+    public DateOnly StartDate { get; set; } = startDate;
     public string Device { get; set; } = device;
-    public string Problem { get; set; } = problem;
-    public string Fullproblem { get; set; } = fullproblem;
+    public string ProblemType { get; set; } = problemtype;
+    public string Description { get; set; } = Description;
     public string Fullname { get; set; } = fullname;
-    public string Statys { get; set; } = statys;
-    public string Master { get; set; } = "Не назначен";
-    public DateTime? EndDate { get; set; } = null;
+    public string Status { get; set; } = status;
+    public string? Master { get; set; } = "Не назначен";
+    public DateOnly? EndDate { get; set; } = null;
     public List<string> Comment { get; set; } = [];
 }
 
-record class OrderUpdateDTO(int Number, string? Statys ="", string? Fullproblem = "", string? Master = "", string? Comment = "");
+record class UpdateOrderDTO(int Number, string? Status ="", string? Description = "", string? Master = "", string? Comment = "");
